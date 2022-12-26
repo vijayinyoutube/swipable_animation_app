f507c2a0ec8bc38328e9de9986ea39c1b70966bb
vijayinyoutube <vijaythevirtue@gmail.com>
2022-12-26 13:29:03 +0530

    ui static commit

--- lib/Presentation/Screens/HomePage/UI/home_page.dart
+++ lib/Presentation/Screens/HomePage/UI/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required this.title});
+  final String title;
 
   @override
   Widget build(BuildContext context) {
